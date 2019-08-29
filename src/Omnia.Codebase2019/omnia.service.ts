bb5a67a51afc820c31d4ee232c1704de858d9f52
andy <andy@ekdahls.net>
2019-08-29 11:42:22 +0200

    Initial Commit with Web Project Setup

---
+++ src/Omnia.Codebase2019/omnia.service.ts
@@ -0,0 +1,21 @@
+import { Composer, DevelopmentEnvironment } from "@omnia/tooling/composers";
+import { Guid } from '@omnia/fx/models';
+
+Composer
+    .registerManifest(new Guid("c79622bf-3341-4f2f-a8c6-579c2dc3fb5c"), "Omnia.Codebase2019")
+    .registerService({ description: "Description of Omnia.Codebase2019" })
+    .AsWebApp()
+    .withBuildOptions({
+        include: ["client"],
+        moduleOptions: {
+            enableTransformResourcePath: true
+        },
+        bundleOptions: {
+            commonsChunk: {
+                name: new Guid("81c58f25-8fe9-46f4-a7c8-9d7fb70f4da1"),
+                minChunks: 2
+            }
+        }
+    });
+    
+   
